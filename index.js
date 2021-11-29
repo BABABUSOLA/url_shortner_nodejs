21a13e16cbfb531d495e2b29a375f7557715ec84
BABABUSOLA <queenbusola1@gmail.com>
2021-11-29 03:50:22 +0100

    commit the backend for the url shortner

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const shortid = require("shortid");
+const validUrl = require("valid-url");
+const shortUrlRoute = require("./routes/shorturl")
+const getShortenUrlRoute = require("./routes/getshortenurl")
+
+const app = express();
+connectDB();
+app.use(cors());
+app.use(express.json({}));
+const PORT = 8000;
+app.listen(PORT, () => console.log("Server is listening on port " + PORT));
+
+
+app.use("/",getShortenUrlRoute)
+app.use("/shorturl", shortUrlRoute);
